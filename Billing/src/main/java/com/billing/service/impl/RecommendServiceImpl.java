86d7f4b91b43e2f01113532a8652269a60d956ac
sunLei <2372388646@qq.com>
2019-05-18 09:14:04 +0800

    sunLei

--- Billing/src/main/java/com/billing/service/impl/RecommendServiceImpl.java
+++ Billing/src/main/java/com/billing/service/impl/RecommendServiceImpl.java
@@ -35,6 +35,7 @@ public class RecommendServiceImpl extends BaseService implements RecommendServic
     private UserMapper userMapper;
     @Autowired
     private BillMapper billMapper;
+
     //用户个性化推荐
     @Override
     public List<Recommend> getRecommendWithUId(Long uId) {
@@ -43,67 +44,79 @@ public class RecommendServiceImpl extends BaseService implements RecommendServic
         int year = now.get(Calendar.YEAR);
         int month = now.get(Calendar.MONTH) + 1;
         Feature feature = featureMapper.selectByMonth(uId, String.valueOf(year), String.valueOf(month));
-        if(feature==null){
-            actionService.calculateFeature(uId,String.valueOf(year),String.valueOf(month));
-            feature=featureMapper.selectByMonth(uId,String.valueOf(year),String.valueOf(month));
+        if (feature == null) {
+            actionService.calculateFeature(uId, String.valueOf(year), String.valueOf(month));
+            feature = featureMapper.selectByMonth(uId, String.valueOf(year), String.valueOf(month));
         }
         print("获取其他用户信息");
         //其他用户信息
-        List<User> users=userMapper.selectAllUsers();
-        Map<Double,Long> userMap=new HashMap<>();//记录对应用户
-        List<Double> distances=new ArrayList<>();
-        List<Feature> features=featureMapper.selectAllList(String.valueOf(year), String.valueOf(month));
-        for(Feature feature1:features){
+        List<User> users = userMapper.selectAllUsers();
+        Map<Double, Long> userMap = new HashMap<>();//记录对应用户
+        List<Double> distances = new ArrayList<>();
+        List<Feature> features = featureMapper.selectAllList(String.valueOf(year), String.valueOf(month));
+        for (Feature feature1 : features) {
             //计算近似度
-            if(feature1.getuId().equals(uId)){
+            if (feature1.getuId().equals(uId)) {
                 //过滤本人信息
                 continue;
             }
             print("计算近似度");
-            double sub1=Math.pow(Double.valueOf(feature.getfParam1())-Double.valueOf(feature1.getfParam1()),2.0);
-            double sub2=Math.pow(Double.valueOf(feature.getfParam2())-Double.valueOf(feature1.getfParam2()),2.0);
-            double sub3=Math.pow(Double.valueOf(feature.getfParam3())-Double.valueOf(feature1.getfParam3()),2.0);
-            double sub4=Math.pow(Double.valueOf(feature.getfParam4())-Double.valueOf(feature1.getfParam4()),2.0);
-            double sub5=Math.pow(Double.valueOf(feature.getfParam5())-Double.valueOf(feature1.getfParam5()),2.0);
-            double sub6=Math.pow(Double.valueOf(feature.getfParam6())-Double.valueOf(feature1.getfParam6()),2.0);
-            double distance=Math.pow(sub1+sub2+sub3+sub4+sub5+sub6,0.5);
+            double sub1 = Math.pow(Double.valueOf(feature.getfParam1()) - Double.valueOf(feature1.getfParam1()), 2.0);
+            double sub2 = Math.pow(Double.valueOf(feature.getfParam2()) - Double.valueOf(feature1.getfParam2()), 2.0);
+            double sub3 = Math.pow(Double.valueOf(feature.getfParam3()) - Double.valueOf(feature1.getfParam3()), 2.0);
+            double sub4 = Math.pow(Double.valueOf(feature.getfParam4()) - Double.valueOf(feature1.getfParam4()), 2.0);
+            double sub5 = Math.pow(Double.valueOf(feature.getfParam5()) - Double.valueOf(feature1.getfParam5()), 2.0);
+            double sub6 = Math.pow(Double.valueOf(feature.getfParam6()) - Double.valueOf(feature1.getfParam6()), 2.0);
+            double distance = Math.pow(sub1 + sub2 + sub3 + sub4 + sub5 + sub6, 0.5);
             print(distance);
             distances.add(distance);
             //允许覆盖:同一距离选取任何一个用户ID都可以,distance可以重复
-            userMap.put(distance,feature1.getuId());
+            userMap.put(distance, feature1.getuId());
         }
         //升序排序
         Collections.sort(distances);
         //构建推荐内容
-        List<Recommend> recommends=new ArrayList<>();
-        for(int i=0;i<3;i++){
-            if(i>=distances.size()){
+        List<Recommend> recommends = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (i >= distances.size()) {
                 continue;
             }
             //前3个最相似用户用户
-            Long userId=userMap.get(distances.get(i));
-            List<Bill> bills=billMapper.selectByUId(userId);
-            for(Bill bill:bills){
-                if(bill.getbType().equals("支出") && bill.getgType4()!=null && bill.getgType()!=null){
-                    String type=bill.getgType();
-                    String brand=bill.getgType4();
-                    List<Recommend> recommends1=recommendMapper.selectByTypeAndBrand(type,brand);
-                    for(Recommend recommend:recommends1){
-                        for(Recommend recommend1:recommends){
-                            if(recommend.getrId().equals(recommend1.getrId())){
+            Long userId = userMap.get(distances.get(i));
+            List<Bill> bills = billMapper.selectByUId(userId);
+            for (Bill bill : bills) {
+                if (bill.getbType().equals("支出") && bill.getgType4() != null && bill.getgType() != null) {
+                    String type = bill.getgType();
+                    String brand = bill.getgType4();
+                    List<Recommend> recommends1 = recommendMapper.selectByTypeAndBrand(type, brand);
+                    for (Recommend recommend : recommends1) {
+                        int flag = 0;
+                        for (Recommend recommend1 : recommends) {
+                            if (recommend.getrId().equals(recommend1.getrId())) {
+                                flag = 1;
                                 break;
                             }
+                        }
+                        if (flag == 0) {
+                            recommends.add(recommend);
                         }
                     }
                 }
             }
         }
-        if(recommends.size()<=10){
-            List<Recommend> recommends1=recommendMapper.selectList();
-            for(Recommend recommend:recommends1){
-                if(Double.valueOf(recommend.getrLike())>0.9){
-                    if(!recommends.contains(recommend)){
-                        recommends.add(recommend);//不重复加入
+        if (recommends.size() <= 10) {
+            List<Recommend> recommends1 = recommendMapper.selectList();
+            for (Recommend recommend : recommends1) {
+                if (Double.valueOf(recommend.getrLike()) > 0.9) {
+                    int flag = 0;
+                    for (Recommend recommend1 : recommends) {
+                        if (recommend.getrId().equals(recommend1.getrId())) {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 0) {
+                        recommends.add(recommend);
                     }
                 }
             }
@@ -113,8 +126,9 @@ public class RecommendServiceImpl extends BaseService implements RecommendServic
 
     @Override
     public List<Recommend> searchByName(String name) {
-        return recommendMapper.selectByLikeName("%"+name+"%");
+        return recommendMapper.selectByLikeName("%" + name + "%");
     }
+
     @Override
     public List<Recommend> getRecommend() {
         return recommendMapper.selectList();
